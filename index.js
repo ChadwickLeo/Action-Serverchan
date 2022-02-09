69a2d7106ec3dcf59ae973e28a11c024ff28069a
emon100 <emon100@qq.com>
2022-02-09 18:06:52 +0800

    v4 release

--- index.js
+++ index.js
@@ -10,55 +10,57 @@ function Post(data, host, headers) {
         timeout: 30000
     };
 
-    let requestCallback = (resolve)=>{
-        return (result)=>{
+    let requestCallback = (resolve) => {
+        return (result) => {
             const encoding = result.headers['content-encoding'];
-            if( encoding === 'undefined'){
+            if (encoding === 'undefined') {
                 result.setEncoding('utf-8');
             }
             let chunks = '';
-            result.on('data', function(chunk) {
-                    try {
-                    chunks+=(chunk);
-                    } catch(e) {
+            result.on('data', function (chunk) {
+                try {
+                    chunks += (chunk);
+                } catch (e) {
                     console.log(e);
-                    }
-                    }).on('end', function(){
-                        if (chunks !== undefined && chunks != null) {
-                        resolve(chunks);
-                        } else {
-                        // 请求获取不到返回值
-                        resolve(opt.host+"无返回值ERROR");
-                        }
-                        })
-        }};
+                }
+            }).on('end', function () {
+                if (chunks !== undefined && chunks != null) {
+                    resolve(chunks);
+                } else {
+                    // 请求获取不到返回值
+                    resolve(host + "ERROR");
+                }
+            })
+        }
+    };
     return new Promise((resolve, reject) => {
-            let cb = requestCallback(resolve);
-            const req = https.request(host, opt, cb);
-            req.on('error', function (e) {
-                    // request请求失败
-                    console.log(opt.host+'请求失败: ' + e.message);
-                    reject("0");
-                    });
-            req.write(data);
-            req.end();
+        let cb = requestCallback(resolve);
+        const req = https.request(host, opt, cb);
+        req.on('error', function (e) {
+            // request请求失败
+            console.log(host + 'ERROR: ' + e.message);
+            reject("0");
+        });
+        req.write(data);
+        req.end();
     });
 }
 
 
 async function run() {
-    try { 
-        let postData = querystring.stringify({
-            text: core.getInput('text',{ required: true }),
-            desp: core.getInput('desp')
-            });
+    try {
+        const postData = querystring.stringify({
+            text: core.getInput('text', {required: true}),
+            desp: core.getInput('desp'),
+            channel: core.getInput('channel')
+        });
 
-        let SCKEY = core.getInput('SCKEY',{ required: true });
-        return Post(postData,`https://sc.ftqq.com/${SCKEY}.send`, {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
-        },'https');
-    } 
+        const sendkey = core.getInput('sendkey', {required: true});
+        return Post(postData, `https://sctapi.ftqq.com/${sendkey}.send`, {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        }, 'https');
+    }
     catch (error) {
         console.log(error);
     }
